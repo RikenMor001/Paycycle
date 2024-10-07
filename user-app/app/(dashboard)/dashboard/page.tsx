8fd965599667303a0c4b2fbaeccb3e0f20a76af8
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2024-10-07 08:47:34 -0400

    Add files via upload

---
+++ user-app/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,15 @@
+
+export default function() {
+    return <div>
+        <div className="flex justify-center items-center h-screen">
+            <div className="rounded-lg shadow-lg bg-white">
+                <div className="text-xl text-[#6a51a6] font-bold">
+                    Welcome to Paytm
+                    <div>
+                        This is where you can send and receive money
+                    </div>
+                </div>            
+            </div>
+        </div>
+    </div>
+}
