5013dc1568c111666cb2711f6481acf56ec4d266
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2024-10-08 19:40:25 -0400

    Update page.tsx

--- user-app/app/(dashboard)/dashboard/page.tsx
+++ user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,15 +1,18 @@
 
+"use client"
 export default function() {
-    return <div>
-        <div className="flex justify-center items-center h-screen">
-            <div className="rounded-lg shadow-lg bg-white">
-                <div className="text-xl text-[#6a51a6] font-bold">
-                    Welcome to Paytm
-                    <div>
-                        This is where you can send and receive money
-                    </div>
-                </div>            
+    return <div className="pt-2">
+            <div className="rounded-lg bg-slate-100 border border-slate-500 px-3 py-3">
+                <div className="flex justify-center items-center">
+                    <button>
+                    Charts
+                    </button>
+                </div>
+            </div>  
+            <div>
+                Actual chart
             </div>
-        </div>
     </div>
 }
+
+
